da60f7687656999688d19aaed3089a3b49eabe00
Michael-Ngochi <ngoochimike@gmail.com>
2025-03-12 10:12:35 +0300

    added functions

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+function saturdayFun(todo){
+    if (todo){
+        return `This Saturday, I want to ${todo}!`
+    }
+    else return "This Saturday, I want to roller-skate!"
+}
+function mondayWork(todo){
+    if (todo){
+        return (`This Monday, I will ${todo}.`)
+    }
+    return "This Monday, I will go to the office."
+}
+console.log(mondayWork())
+
+function wrapAdjective(flair){
+if (flair){
+    flair=flair
+}else flair="*"
+
+   return function(adjective){
+        if (adjective){
+            return `You are ${flair}${adjective}${flair}!`
+        }
+        else return `You are ${flair}special${flair}!`
+    }
+}
+
+//console.log(wrapAdjective("@@@")("Odd"));
+console.log(wrapAdjective("!!!")("strange"));
+console.log(wrapAdjective("%")("a dedicated programmer"));
